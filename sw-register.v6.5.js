65708a1be678fc44a33bb8ce312b0b33da8164c1
opesqueraa-ok <opesqueraa@miumg.edu.gt>
2025-10-04 12:12:25 -0600

    Update and rename sw-register.v6.5.js to sw-register.v6.6.js

--- sw-register.v6.5.js
+++
@@ -1,7 +0,0 @@
-(function(){
-  if('serviceWorker' in navigator){
-    window.addEventListener('load', () => {
-      navigator.serviceWorker.register('./sw.v6.5.1.js?v=6.5.1');
-    });
-  }
-})();
