4fe8e6a8433991522d4be0e0801aff5422b3ff56
opesqueraa-ok <opesqueraa@miumg.edu.gt>
2025-10-12 19:28:12 -0600

    Update sw-register.v7.0.js

--- sw-register.v7.0.js
+++ sw-register.v7.0.js
@@ -1,7 +1,7 @@
-(function () {
-  if ('serviceWorker' in navigator) {
-    window.addEventListener('load', () =>
-      navigator.serviceWorker.register('./sw.v7.0.js?v=7.0')
-    );
+(function(){
+  if('serviceWorker' in navigator){
+    window.addEventListener('load', ()=>{
+      navigator.serviceWorker.register('./sw.v7.0.js?v=7.0');
+    });
   }
 })();
